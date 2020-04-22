095f71250c303cb711f3d56e9cace49bfb8cfd46
Theo <theoMhalpern@gmail.com>
2020-04-22 22:32:34 +0000

    Initial commit

---
+++ books.js
@@ -0,0 +1,1 @@
+var b = document.getElementsByClassName("page6");
---
+++ script.js
@@ -0,0 +1,59 @@
+var prompts = ["1. Dialectical Journal – choose 3 quotes from the section that you read. Copy the quote in one column. Write your analysis of your selections in the other. What are your thoughts, comments, ideas, and understandings about the significance of the chosen parts of the text?","2. What is the writer’s purpose? What does he/she hope to accomplish? Choose at least two passages from the text that clearly demonstrate said purpose.","3. Who is the author’s intended audience? What group or subgroup of people is the author trying to persuade or inform? What assumptions about the audience does the writer make?","4. How does the author establish his or her own authority (ethical appeal)? What does he/she do to prove he/she is a credible source? If mentioned, how did he/she conduct his/her research prior to writing? Didyou find the information in this book to be reliable? How do you define “reliable?” How did the author prove that his/her facts were true?","5. What claims does the writer make? How does he/she support them? Do you ultimately find him/her convincing? What is the author’s primary method of persuading his/her audience?","6. Explain some things you have learned so far that you are not likely to forget in the near future.","7. What questions does this book raise for you? What issues are addressed or ignored in the text?","8. Did the author weave opinion into facts? Can you find examples of each? Explain.","9. What patterns doyou see emerging in the text? Does the author repeat certain words or phrases? Sentence patterns? Symbols? Images? Modes of narration?","10. What connections can you make from the book to movies, news events, other texts, your own life, etc.?","11. Ifyour book has a main character, does he/she change over the course of the text? If so, why and how?","12. Write about something that intrigues, impresses, amuses, shocks, puzzles, disturbs, repulses or aggravates you.","13. Look back at the vocabularywords you have circled throughout your reading. Choose ten words that you really like and want to remember for future writingand speaking. Explain your affinity for the word. Elaborate onthe word: its definition, words that are synonymous with it, the roots of the word, the history of the word, other words thatyou can relate to it.","14. Choose a passage from the text that you think is just outstanding writing. Include the passage and articulate why you find this selection so powerful.","15. Didthis book make you laugh? Cry? Cringe? Smile? Cheer? Explain.","16. What is the most important word in the book? The most important passage? The most important event or feeling? Explain.","17. What parts of the book seem most believable or unbelievable? Why?","18. Do any of the characters remind you of friends, family members, or classmates? Explain.","19. What would you andyour favorite character talk about in your conversation? Beginthe conversation.","20. Do you think the title fits the book? Why or why not?","21. What was the author saying about life andliving through this book? In other words, what is the theme ofthe book? Cite specific examples of how that theme is developed.","22. If I was the author, I’d change….","23. Write a poem: A. From a character’s viewpoint B. About the setting C. About acharacter D. About the theme of the book","24. Write a eulogy ( a speech honoring someone after death) for one of the characters.","25. Create a slogan for the book and explain why you chose this…","26. Writing a commercial script for this book.","27.If this book would become a movie, choose 5 main scenes and why you chose them.","28. If this book were to become a movie, who would you cast to play the main characters. Explain your choices with evidence from the text.","29. This book is controversial because…..","30. This story helps me understand the problem of…better because…..","31. In the sequel to this book, the following should happen…..","32. Give 3 reasons why this book should be taught to a whole class. Support each reason with ample evidence from the book.","33. If the book were set 50 years in the future or the past, how would the conflict change?","34. Create an award for this book. Explain the award and why this book received it.","35. Discuss how…(character) shows his/her personality through the dialogue in the story.","36. Discuss how…..(character) shows his/her personality by the actions he/she takes.","37. Discuss how…..(character) shows his/her personality by what other characters say or do toward this character.","38. Who is the narrator in the story and why do you think the author chose this narrator?","39. Is the story written in the first person? Third? Give an example from the book to support your pickof point of view.","40. Did the author switch between narrators? Who are they? Why do you think they did that?","41. What particular style does the author use in her/his writing? Do you like or dislike it? Why?","42. What do you remember most about the story?","43. How do the chapter titles help you predict what might happen next? Are the chapter titles appropriate?","44. What was the main problem in the story?","45. Were there other less significant conflicts in the story? Discuss the role they play in the story.","46. Describe a conflict and identify it as either internal or external. Tell why you chose that label.","47. Were there clues that led you to believe that something wouldhappen?","48. Discuss a particular event in the book that usedforeshadowing or flashbacks.","49. Does the author use shifts in time? Why?","50. How did the author leave the reader? Was the ending satisfying? Explain.","51. Was there an epilogue in the book? How was it used? What effect did it have on the story?","52. Did you like the way the author ended the story? Make up a new ending that could still fit with the previous events in the plot.","53. Did the story end the way you expected it to? What clues did the author offer to prepare you to expect this ending? Did you recognize these clues as important to the story asyou were first reading it?","54. Would you rather guess the ending or be surprised? Were you able to guess the ending? Did you like the way it ended? How would you have changed the ending?Explain.","55. Where does the story take place? Tell what the place is like. If the story took place somewhere else or in a different time, how would it be changed?","56. Compare the setting to a place you have been before, or a place you hope to visit.","57. Tell about your reaction to a particularly interestingpassage where the author creates an image of the setting.","58. Compare where you live with the neighborhood, town or city inthe book. Would you rather live in the place where this story takes place? Why or why not?","59. Would you like to live during the time this story takes place? Why?","60. Describe the personality of a particular character. Include examples from the story that support your description.","61. Do any of the characters change during the story? If so, how are they different? Whatchanged them? Did it seem believable?","62. How did the main character change from the beginning to the end of the story?","63. Select a relatively important event in the book and describewhy it was a turning point in the character’s life? What was it that the character decided or learned to do?","64. Pick a paragraph or a page which describes the way another character treats the main character. Would you treat the person the same way?Find an incident in your own life which is similar and explainwhy you treated someone the way you did. Have you changed fromthat experience? How would you treat that person now?","65. Often main characters will grow in confidence and learn how to accept themselves in some new way. Cite an example of how the protagonist found a source of strength. Describe how you felt as the reader when this happened.","66. List the characters in the story and describe qualities which you admire. Tell what and why you respect the individuals. Point out classmates who possessthe traits you admire.","67. Do any of the characters do things you feel are wrong? Explain.","68. Some characters play smallbut important roles in the story. Name such a character. Why is this character necessary for the story?","69. Would you like to be anyone in the story? Who? Why?","70. Think about the characters in the story. Are any of them the same type of characters you have met in other stories, but not the same series? Explain."];
+
+function myFunction(x) {
+  var dots = document.getElementsByClassName("dots")[x];
+  var moreText = document.getElementsByClassName("more")[x];
+  var btnText = document.getElementsByClassName("myBtn")[x];
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function allNone() {
+	var dots = document.getElementsByClassName("dots");
+  var moreText = document.getElementsByClassName("more");
+  var btnText = document.getElementsByClassName("myBtn");
+	var button = document.getElementById("exp");
+	if (button.innerHTML == "Expand All") {
+		for (i = 0; i < dots.length; i++) {
+			dots[i].style.display = "none";
+			btnText[i].innerHTML = "Read less";
+			moreText[i].style.display = "inline";
+			button.innerHTML = "Contract All";
+		}
+  } else {
+    for (i = 0; i < dots.length; i++) {
+			dots[i].style.display = "inline";
+			btnText[i].innerHTML = "Read more";
+			moreText[i].style.display = "none";
+			button.innerHTML = "Expand All";
+		}
+  }
+}
+
+
+function newPrompt() {
+	var place = document.getElementById("prom");
+	var promptt = Math.floor(Math.random() * prompts.length);  
+	place.innerHTML = prompts[promptt];
+}
+
+function oldNew()	{
+	var oldd = document.getElementById("oldd");
+	var left = document.getElementsByClassName("leftcolumn");
+	if (oldd.innerText=="Oldest First") {
+		left[0].style.flexDirection = "column";
+		oldd.innerText = "Newest First";
+	}
+	else {
+		left[0].style.flexDirection = "column-reverse";
+		oldd.innerText = "Oldest First";
+	}
+}
